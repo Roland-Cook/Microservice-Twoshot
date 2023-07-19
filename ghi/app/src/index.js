6636e01797c3db8720c5e1417434b8845bd4df45
Nick <NicholasDRuss@gmail.com>
2023-07-19 18:21:33 -0400

    Index test

--- ghi/app/src/index.js
+++ ghi/app/src/index.js
@@ -3,8 +3,19 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+async function loadHats() {
+  const response = await fetch('http://localhost:8090/api/hats');
+  if (response.ok) {
+    const data = await response.json();
+      root.render(
+        <React.StrictMode>
+          <App hats={data.hats}/>
+        </React.StrictMode>
+      );
+  } else {
+    console.error(response);
+  }
+}
+
+loadHats();
