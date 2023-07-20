a7ab9a70de7e39fe522a4bc4459171166ec6d02b
Nick <NicholasDRuss@gmail.com>
2023-07-20 18:02:57 -0400

    Added logos and main page

--- ghi/app/src/MainPage.js
+++ ghi/app/src/MainPage.js
@@ -1,13 +1,13 @@
 import "./index.css"
+import logo1 from "./images/logo1.png"
 
 function MainPage() {
   return (
     <div className="px-4 py-5 my-5 text-center">
-      <h1 className="display-5 fw-bold">WARDROBIFY!</h1>
+      
       <div className="col-lg-6 mx-auto">
         <p className="lead mb-4">
-          Need to keep track of your shoes and hats? We have
-          the solution for you!
+          <img className="logo-main-page" src={logo1} />
         </p>
       </div>
       <img className="main-page-image" alt="img"  src="https://img.freepik.com/premium-photo/interior-room-monochrome-blue-color-with-clothes-hanger-room-accessories_576429-1246.jpg?w=2000"/>
